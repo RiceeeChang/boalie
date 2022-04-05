c1e66900dda033aff5bc73b29ed8ce349c0abbb4
Rice <riceeechang@gmail.com>
2022-04-06 01:32:53 +0800

    客製化typeform like表單

---
+++ js/signup.js
@@ -0,0 +1,44 @@
+
+
+var liffID = '1656721259-jdXZ4Vqx';
+var loginUrl = 'https://riceeechang.github.io/boalie/';
+var lineAtUrl = 'https://lin.ee/i5P97Pv';
+var formUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSe9qd9fyDhCmNLHWiuR9qjblX_apwe_RKUb2Wm7kqLU3JdoLg/viewform?usp=pp_url&entry.697677827=';
+
+liff.init({
+	liffId: liffID
+}).then(function() {
+	var isInLine = liff.isInClient();   // 回傳是否由 LINE App 存取
+	if (!isInLine) {
+		// 顯示請他使用手機的Line掃QRCode的方式點開
+		console.log("請使用手機的Line開啟");
+	}
+
+	var isLoggedIn = liff.isLoggedIn(); // 使用者是否登入 LINE 帳號
+    var context = liff.getContext();
+	if (isLoggedIn) {
+		liff.getProfile()
+			.then(profile => {
+                var userId = profile['userId'];
+				// 驗證line_id是否已經加入
+				fetch('https://script.google.com/macros/s/AKfycbzzNhSUe4bmfL5-Fd5Fy0zzlMJtWM9YZIWURTFd8gVxyfpC9jXGM5uGcgszT9W0sKj0yg/exec?line_id='+userId)
+				.then(function(response) {
+					return response.json();
+				})
+				.then(function(response) {
+					if (response.result == true) { // 已經加入，直接轉去Line@
+						location.href = lineAtUrl;
+					} else { // 還沒加入，轉去註冊表單
+						location.href = formUrl + userId;
+					}
+				});
+				console.log(profile, userId);
+			})
+	} else {
+		liff.login({
+			redirectUri: loginUrl // 使用者登入後要去到哪個頁面
+		});
+	}
+}).catch(function(error) {
+  console.log(error);
+});
